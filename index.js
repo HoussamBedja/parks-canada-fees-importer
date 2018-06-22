077f0b892278727bb3bc398d59d23bce2df19718
Houssam <houssam.b@hotmail.com>
2018-06-22 16:48:39 -0400

    added JSON request formatting function

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var fs = require('fs');
 var request = require('request');
 var xml2js = require('xml2js');
 var json2csv = require('json2csv');
+const util = require('util')
 
 function fetchDataSet(name, url) {
     console.log('Fetching \'' + name + '\': ' + url);
@@ -27,29 +28,45 @@ function fetchDataSet(name, url) {
         }
 
         var xml = body;
-        var parser = new xml2js.Parser();
+        var parser = new xml2js.Parser({explicitArray: false});
         parser.parseString(xml, function(error, results) {
             var subfeesData = results['subfees.xml']['I_SUBFEES'];
-
+            var results = format(subfeesData)
+            //console.log(results));
+            //console.log(util.inspect(results, false, null))
             for (var dataPointIndex in subfeesData) {
                 var dataPoint = subfeesData[dataPointIndex];
                 for (var key in dataPoint) {
                     if (dataPoint.hasOwnProperty(key)) {
-                    console.log('key:' + key + '; value: ' + dataPoint[key]);
+                    //console.log('key:' + key + '; value: ' + dataPoint[key]);
                     }
                 }
             }
-
-            var json2csvParser = new json2csv.Parser();
-            var csv = json2csvParser.parse(subfeesData);
-            fs.writeFile(name + '.csv', csv, function(error) {
-                if (error) {
-                    console.log('Unable to write to file: ' + error);
-                }
-            });
         })
     });
 }
+
+
+function format(data) {
+
+    var results = [];
+    for (index in data) {
+        var item = {};
+        for (key in data[index]) {
+            item[key] = {};
+            item[key]["S"] = data[index][key];
+        }
+        console.log(item);
+        var putRequest = {};
+        putRequest.Item = item;
+        var dataset = {};
+        dataset.PutRequest = putRequest;
+        results.push(dataset);
+    }
+
+    return results;
+}
+
 
 for (name in DATA_SETS) {
     var url = DATA_SETS[name];
